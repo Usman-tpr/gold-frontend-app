6a0fb713f2ace98201dee8fe1bbc01b28bb8c632
Usman Ali <Usman.tpr@gmail.com>
2024-12-02 19:42:01 +0500

    added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
         <div className="flex items-center justify-between h-16">
           <div className="flex-shrink-0">
             <a href="/" className="text-2xl font-bold text-gray-800">
-              Jewelry<span className="text-yellow-500">Shop</span>
+              Jewery<span className="text-yellow-500">Shop</span>
             </a>
           </div>
 
